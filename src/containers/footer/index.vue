1dbbad6fdec7a197f4291b7431b68c64407ce37d
Pablo Barral <pablo.barral.kts@gmail.com>
2022-02-20 20:19:04 -0300

    Feat: footer structure set up

--- src/containers/footer/index.vue
+++ src/containers/footer/index.vue
@@ -1,9 +1,38 @@
 <template>
-  
+  <footer>
+      <TextContent :style="{'grid-column': 2}" footerText=true 
+      text="Essa janela do navegador
+       é usada para manter sua sessão de autenticação ativa. 
+       Deixe-a aberta em segundo plano e abra uma nova janela para 
+       continuar a navegar."/>
+       <span/>
+       <TextContent :style="{'grid-column': 4, 'text-align': 'right'}" timerText=true text="Application refresh in" />
+       <div :style="{'grid-column': 6}" id="timerBox">
+           <div>{{ seconds }}</div>
+           <TextContent timerText=true text="seconds" />
+       </div>
+       <TextContent id="continueText"  :style="{'grid-column': 7}" continueText=true text="Continuar Navegando" />
+       <button :style="{'grid-column': 8}">Logout</button>
+  </footer>
 </template>
 
 <script>
+
+import TextContent from '@/components/textContent'
+
 export default {
+    name: "Footer",
+    components:{
+        TextContent
+    },
+
+    data(){
+        return{
+            seconds: 600
+        }
+    },
+
+
 
 }
 </script>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <Header/>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import Header from '@/containers/header'
+import Footer from '@/containers/footer'
 
 export default {
     name:"Home",
     components: {
       Header,
+      Footer,
     },
 }
 </script>

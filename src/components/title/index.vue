f95a3e7c39a10eda4790b9f40a84091985aaec8a
Pablo Barral <pablo.barral.kts@gmail.com>
2022-02-21 11:02:48 -0300

    Style: home background, spacing and dynamic text classes ajusted

--- src/components/title/index.vue
+++ src/components/title/index.vue
@@ -1,20 +1,50 @@
 <template>
-  <h1 v-if="size === '1'" :style="{color: textColor}">
+  <h1 v-if="size === '1'" 
+  :style="{color: textColor}" 
+  :class="{
+    firstTitle: homeFirstTitleData,
+    homeTitleStyle: homeTitleData
+  }">
     {{ text }}
   </h1>
-  <h2 v-else-if="size === '2'" :style="{color: textColor}">
+  <h2 v-else-if="size === '2'" 
+  :style="{color: textColor}" 
+  :class="{
+    firstTitle: homeFirstTitleData,
+    homeTitleStyle: homeTitleData
+  }">
     {{ text }}
   </h2>
-  <h3 v-else-if="size === '3'" :style="{color: textColor}">
+  <h3 v-else-if="size === '3'" 
+  :style="{color: textColor}" 
+  :class="{
+    firstTitle: homeFirstTitleData,
+    homeTitleStyle: homeTitleData
+  }">
     {{ text }}
   </h3>
-  <h4 v-else-if="size === '4'" :style="{color: textColor}">
+  <h4 v-else-if="size === '4'" 
+  :style="{color: textColor}" 
+  :class="{
+    firstTitle: homeFirstTitleData,
+    homeTitleStyle: homeTitleData
+  }">
     {{ text }}
   </h4>
-  <h5 v-else-if="size === '5'" :style="{color: textColor}">
+  <h5 v-else-if="size === '5'" 
+  :style="{color: textColor}" 
+  :class="{
+    firstTitle: homeFirstTitleData,
+    homeTitleStyle: homeTitleData
+  }">
     {{ text }}
   </h5>
-  <h6 v-else-if="size === '6'" :style="{color: textColor}">
+  <h6 v-else-if="size === '6'" 
+  :style="{color: textColor}" 
+  :class="{
+    firstTitle: homeFirstTitleData,
+    homeTitleStyle: homeTitleData
+  }">
     {{ text }}
   </h6>
 </template>
@@ -34,9 +64,22 @@ name:'Title',
    },
    textColor:{
      type: String,
-   }
+   },
+   homeFirstTitle:{
+     default: false,
+   },
+   homeTitle:{
+     default: false,
+   },
 
- }
+ },
+
+ data(){
+   return{
+     homeFirstTitleData: this.homeFirstTitle,
+     homeTitleData: this.homeTitle
+   }
+ },
 }
 </script>
 

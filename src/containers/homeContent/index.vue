ec6986c16f8ab5d00f95681cf0d011efbae03dee
Pablo Barral <pablo.barral.kts@gmail.com>
2022-02-20 23:01:22 -0300

    Feat: added all of home content and fixed issue with footer gird template

--- src/containers/homeContent/index.vue
+++ src/containers/homeContent/index.vue
@@ -2,10 +2,21 @@
   <div>
       <img src="@/assets/ball-LogoCompass.png" alt="Compass logo">
       <div>
-          <div id="firstParagraph">
-            <Title size=2 text="Our mission is" textColor="#C12D18"/>
-            <TextContent homeParagraph=true text="Nossa missão é"/>
-          </div>
+            <div id="firstParagraph">
+                <Title size=2 text="Our mission is" textColor="#C12D18"/>
+                <TextContent homeParagraph=true text="Nossa missão é"/>
+            </div>
+
+            <Title size=3 text="to transform the world" textColor="#C12D18"/>
+            <TextContent homeParagraph=true text="transformar o mundo"/>
+
+            <Title size=3 text="building digital experiences" textColor="#C12D18"/>
+            <TextContent homeParagraph=true text="construindo experiências digitais"/>
+
+            <Title size=3 text="that enable our client’s growth" textColor="#C12D18"/>
+            <TextContent homeParagraph=true text="que permitam o crescimento dos nossos clientes"/>
+
+            
       </div>
   </div>
 </template>

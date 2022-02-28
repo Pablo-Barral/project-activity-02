742e17ee621b944879c8ddffc5369755e8df50f0
Pablo Barral <pablo.barral.kts@gmail.com>
2022-02-27 23:01:40 -0300

    Test: refactored weather test

--- src/components/__tests__/weather.spec.js
+++ src/components/__tests__/weather.spec.js
@@ -3,10 +3,11 @@ import { shallowMount } from '@vue/test-utils'
 
 describe('index.vue', () => {
   it('renders weather icon when passed', async () => {
-    const iconShow = ''
+      const iconShow = 'cloudy'
+      const mockSource = require(`@/assets/${iconShow}.png`)
     const wrapper = shallowMount(weather, {
       propsData: { iconShow },
     }) 
-    expect(wrapper.find('img').attributes().src).toEqual(iconShow)
+    expect(wrapper.find('img').attributes().src).toEqual(mockSource)
   })
 })

275c8d09eeccbec9fcbaea67814aa4f948d7f3d8
Pablo Barral <pablo.barral.kts@gmail.com>
2022-02-28 18:04:57 -0300

    Build: added vue-sanitize

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
+import sanitize from 'vue-sanitize'
 
 Vue.config.productionTip = false
 
@@ -10,5 +11,6 @@ new Vue({
   router,
   store,
   axios,
+  sanitize,
   render: h => h(App)
 }).$mount('#app')
